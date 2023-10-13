62304c67c05fffca99a0a205b9af223383cdfe77
Sarthak Oza <Sarthak.Oza@tlgcohort.com>
2023-10-13 07:50:37 -0700

    update port

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ app.use(express.json());
 
 app.use("/api/students", studentRouter);
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("Listening"));
